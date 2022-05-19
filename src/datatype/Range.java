87bb651282179bb5abb2ff5331728bf5046347a2
Prakash <prakash@gmail.com>
2022-05-19 20:56:43 +0530

    update

---
+++ src/datatype/Range.java
@@ -0,0 +1,32 @@
+package datatype;
+
+
+public class Range {
+ public static void main(String[] args) {
+	
+	 byte b=45;
+	System.out.println("my rank is:"+b);
+	
+	short s =1993;
+	System.out.println("My birth year is ;"+s);
+	
+	int i=603302;// int premitive type is store maximum 9 digit
+	System.out.println("my Area pincode is m:"+i);
+	
+	long l= 77083034919l;//long  is store maximum 19 digit
+	System.out.println(" my mobile nu,ber is :"+l);
+	
+	float f= 6.2123409f;// float is store the 7 digit of the after decimal point and before maximum 9 digit
+	System.out.println("my college CGPA is :"+f);
+	
+	double d= 356/12d;
+	System.out.println("the ivision of 356/12 is:"+d);
+	
+	char c='M';
+	System.out.println("My gender is :"+c);
+	
+	boolean g=true;
+	System.out.println("this above coding is true or false:"+g);
+ 
+ }	
+}
